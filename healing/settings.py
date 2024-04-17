ef24330d368452fee0f8c049bccd79605d5878a1
RODRIGO CASTRO <rodrigobfcastro@gmail.com>
2024-04-17 20:56:01 -0300

    Second Commit: Paciente, Agendamento, Consultas, Consultas Medicas

--- healing/settings.py
+++ healing/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
-    'medico'
+    'medico',
+    'paciente'
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- medico/models.py
+++ medico/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 def is_medico(user):
     return DadosMedico.objects.filter(user=user).exists()
@@ -27,3 +28,16 @@ class DadosMedico(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    @property
+    def proxima_data(self):
+        proxima_data = DatasAbertas.objects.filter(user=self.user).filter(data__gt=datetime.now()).filter(agendado=False).order_by('data').first()
+        return proxima_data
+    
+class DatasAbertas(models.Model):
+    data = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    agendado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.data)
